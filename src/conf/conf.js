e1340770da25668281620f73455a2f35051ee6f6
developerjay18 <jayrai3003@gmail.com>
2024-03-21 12:10:27 +0530

    removing cateogory env files

--- src/conf/conf.js
+++ src/conf/conf.js
@@ -2,9 +2,6 @@ const conf = {
   appwriteUrl: String(import.meta.env.VITE_APPWRITE_URL),
   appwriteProjectId: String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
   appwriteDatabaseId: String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
-  appwriteCategoryCollectionId: String(
-    import.meta.env.VITE_APPWRITE_CATEGORY_COLLECTION_ID
-  ),
   appwriteExpenseCollectionId: String(
     import.meta.env.VITE_APPWRITE_EXPENSE_COLLECTION_ID
   ),
